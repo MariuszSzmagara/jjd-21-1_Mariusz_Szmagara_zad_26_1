59cf616ec37cfbb01261dbd16862bd4f7f476aa0
MariuszSzmagara <mariusz.szmagara@gmail.com>
2021-05-04 17:46:21 +0200

    Initial commit

---
+++ src/main/java/pl/javastart/cookbook/Jjd211MariuszSzmagaraZad261Application.java
@@ -0,0 +1,13 @@
+package pl.javastart.cookbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jjd211MariuszSzmagaraZad261Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jjd211MariuszSzmagaraZad261Application.class, args);
+    }
+
+}
